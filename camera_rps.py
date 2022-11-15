1dce9f20d5a790c514c41420e6fc2f58b83ddfa3
Luke Tutton <tuttonluke@gmail.com>
2022-11-15 10:16:26 +0000

    Adds 3 second timer and best-of-3 win condition.

--- camera_rps.py
+++ camera_rps.py
@@ -2,11 +2,35 @@
 import cv2
 import numpy as np
 from keras.models import load_model
+import time
+import random
 #%%
+def get_computer_choice() -> int:
+    """Randomly chooses one of three options:
+    0: Rock
+    1: Paper
+    2: Scissors
+
+    Returns:
+        int: Random choice of 0, 1, or 2, corresponding to roc, paper, and
+        scissors, respectively.
+    """
+    return random.randint(0,2)
+
 def get_prediction() -> int:
+    """Predicts the user's choice of rock, paper, or scissors via webcam
+    using tensorflow model prediction.
+
+    Returns:
+        int: 0 Rock
+             1 Paper
+             2 Scissors
+             3 Nothing
+    """
     model = load_model('keras_model.h5', compile=False)
     cap = cv2.VideoCapture(0) # webcam referenced by integer 0 argument
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    time_1 = time.time()
 
     while True: 
         ret, frame = cap.read()
@@ -17,10 +41,13 @@ def get_prediction() -> int:
         prediction = model.predict(data)
         cv2.imshow('frame', frame)
         # Press q to close the window
-        print(prediction)
+        # print(prediction)
+        time_2 = time.time()
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-                
+        elif time_2 - time_1 > 3:
+            break
+        
     # After the loop release the cap object
     cap.release()
     # Destroy all the windows
@@ -28,12 +55,60 @@ def get_prediction() -> int:
 
     return prediction[0].argmax()
 
-#%%
-user_input = get_prediction()
+def get_winner(computer_choice, user_choice) -> int:
+    """Compares output of get_computer_choice and get_user_choice functions
+    and returns an integer corresponding to the three outcomes of tie (0),
+    computer win (1), or user win(2).
 
-print(user_input)
-# for i in user_input:
-#     print(i)
+    Args:
+        computer_choice (int): computer's choice of rock, paper, or scissors.
+        user_choice (int): user's choice of rock, paper, or scissors.
+
+    Returns:
+        int: 0 (Tie)
+             1 (Computer win)
+             2 (User win)
+    """
+    if computer_choice == user_choice:
+        return 0
+    elif (
+        (computer_choice == 0 and user_choice == 2) or
+        (computer_choice == 1 and user_choice == 0) or
+        computer_choice == 2 and user_choice == 1
+        ):
+        return 1
+    else:
+        return 2
+
+def play_rps():
+    """Plays the game of rock-paper-scissors, winner is first to three.
+    Prints a statement indicating the winner.
+    """
+    computer_wins = 0
+    user_wins = 0
+    choices_dict = {0: "rock", 1 : "paper", 2 : "scissors"}
+
+    while True:
+        computer_choice = get_computer_choice()
+        print(choices_dict.get(computer_choice))
+        user_choice = get_prediction()
+        if user_choice < 3:
+            print(f'You chose {choices_dict.get(user_choice)}')
+            result = get_winner(computer_choice, user_choice)
+            if result == 1:
+                computer_wins += 1
+            elif result == 2:
+                user_wins += 1
+        else:
+            print('Input not recognised.')
+        if computer_wins == 3:
+            print(f"Computer wins {computer_wins} to {user_wins}.")
+            break
+        elif user_wins == 3:
+            print(f"User wins {user_wins} to {computer_wins}.")
+            break
 #%%
-my_array = np.ndarray(shape=(1,2,2,3), dtype=int)
-zeros_array = np.zeros(shape=(3,4))
+play_rps()
+
+#%%
+time.time()
